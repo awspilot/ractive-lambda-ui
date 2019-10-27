4fd6a83a2f2344c0eda9f02754064220d15d999a
adrianpraja <adrian@databank.ro>
2019-10-27 12:01:53 +0200

    update

--- src/functioncreate.js
+++ src/functioncreate.js
@@ -1,7 +1,20 @@
 
 export default Ractive.extend({
 	template: `
-		create function form
+		<h3>Create function</h3>
 
+		<div style="box-shadow: 0 1px 1px 0 rgba(0,28,36,.5);border-top: 1px solid #eaeded;background-color: #fff">
+			create function form
+		</div>
+
+		<div style="text-align: right;padding: 10px 0px;">
+				<a class="btn btn-sm" on-click="cancel"> Cancel </a>
+				<a class="btn btn-sm btn-warning" on-click="createfunction"> Create function </a>
+		</div>
 	`,
+	on: {
+		cancel() {
+			this.parent.gotolist()
+		}
+	}
 })
