d44e4daf6bda73c75479192dd4895b025935274f
adrianpraja <adrian@databank.ro>
2020-01-11 16:28:00 +0200

    +

---
+++ src/aws_left.js
@@ -0,0 +1,17 @@
+
+export default Ractive.extend({
+	template: `
+			<div style="height: 100%;border-right: 1px solid #b9b8b6;background-color: #fff;">
+				<div style="display: block;font-size: 12px;padding-left: 10px;font-size: 18px;font-weight: 700;color: #000;line-height: 2rem;padding: 12px 35px;border-bottom: 1px solid #ddd;">
+					AWS Lambda
+				</div>
+				<div style="position: absolute;bottom: 0px;top: 60px;left: 0px;right: 0px;">
+
+					<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Dashboard</div>
+					<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Applications</div>
+					<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #ec7211;font-weight: bold;">Functions</div>
+					<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Layers</div>
+				</div>
+			</div>
+`
+})
--- src/index.js
+++ src/index.js
@@ -9,20 +9,20 @@
 	import functioncreate from './functioncreate'
 	import functiondetail from './functiondetail'
 
+	import AWSLeft from './aws_left';
+
 	export default Ractive.extend({
+		components: {
+			functionlist: functionlist,
+			functioncreate: functioncreate,
+			functiondetail: functiondetail,
+
+			AWSLeft: AWSLeft,
+		},
 		template: `
 	<hsplit class='ractive-lambda-ui theme-{{theme}}'>
-		<left style="border-right: 1px solid #b9b8b6;background-color: #fff;">
-			<div style="display: block;font-size: 12px;padding-left: 10px;font-size: 18px;font-weight: 700;color: #000;line-height: 2rem;padding: 12px 35px;border-bottom: 1px solid #ddd;">
-				AWS Lambda
-			</div>
-			<div style="position: absolute;bottom: 0px;top: 60px;left: 0px;right: 0px;">
-
-				<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Dashboard</div>
-				<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Applications</div>
-				<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #ec7211;font-weight: bold;">Functions</div>
-				<div style="display: block;height: 30px;line-height: 30px;font-size: 13px;padding: 0px 35px;border-top: 1px solid transparent;border-left: 1px solid transparent;margin-bottom: 0px;color: #000;">Layers</div>
-			</div>
+		<left>
+			<AWSLeft />
 		</left>
 		<content  style="background-color: transparent;border: 0px;overflow-x: auto;">
 
@@ -62,11 +62,7 @@
 	</hsplit>
 
 `,
-		components: {
-			functionlist: functionlist,
-			functioncreate: functioncreate,
-			functiondetail: functiondetail,
-		},
+
 		data: function() {
 			return {
 				tab: 'list',
