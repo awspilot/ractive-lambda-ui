5a529ca09dc57105aec4f8348db0d145b7202d31
adrianpraja <adrian@databank.ro>
2020-02-09 13:21:49 +0200

    dist

--- dist/ractive-lambda-ui.js
+++ dist/ractive-lambda-ui.js
@@ -19672,22 +19672,131 @@ exports.default = Ractive.extend({
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
+var sizes = [128, 192, 256, 320, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024, 1088, 1152, 1216, 1280, 1344, 1408, 1472, 1536, 1600, 1664, 1728, 1792, 1856, 1920, 1984, 2048, 2112, 2176, 2240, 2304, 2368, 2432, 2496, 2560, 2624, 2688, 2752, 2816, 2880, 2944, 3008];
+
 exports.default = Ractive.extend({
-	template: '\n\t\t<pre>\n{{  debug }}\n\t\t</pre>\n\t',
+	template: '\n\n\t\t<!-- env -->\n\t\t<div style="width: min-height: 300px;padding: 10px;">\n\t\t\t<div style="background-color: #fff;height: 100%;padding: 10px;">\n\t\t\t\t<h3>Environment variables</h3>\n\n\t\t\t\t<table>\n\t\t\t\t{{#each updateFunctionConfiguration.Configuration.Environment.Variables:key }}\n\t\t\t\t\t<tr>\n\t\t\t\t\t\t<td> <input value={{key}} /></td>\n\t\t\t\t\t\t<td> <input value={{.}}   /></td>\n\t\t\t\t\t\t<td></td>\n\t\t\t\t\t</tr>\n\t\t\t\t{{/each}}\n\t\t\t\t</table>\n\t\t\t</div>\n\t\t</div>\n\n\t\t<div style="width: 50%;float: left;height: 300px;padding: 10px;">\n\t\t\t<div style="background-color: #fff;height: 100%;padding: 10px;">\n\t\t\t\t<h3>Execution role</h3>\n\n\t\t\t\t{{getFunction.Configuration.Role}}\n\t\t\t</div>\n\t\t</div>\n\n\t\t<div style="width: 50%;float: left;height: 300px;padding: 10px;">\n\t\t\t<div style="background-color: #fff;height: 100%;padding: 10px;">\n\t\t\t\t<h3>Basic Settings</h3>\n\n\t\t\t\t<label>Description</label>\n\t\t\t\t<div style="margin-bottom: 15px;">\n\t\t\t\t\t<input type="text" class="input-text" style="width: 100%;" value={{updateFunctionConfiguration.Configuration.Description}} />\n\t\t\t\t</div>\n\n\t\t\t\t<label>Memory (MB) - {{updateFunctionConfiguration.Configuration.MemorySize}}</label>\n\t\t\t\t<div style="margin-bottom: 15px;">\n\t\t\t\t\t<input type="range" value={{memsize}} style="width: 100%;" min="0" max={{sizes.length-1}}>\n\t\t\t\t</div>\n\n\t\t\t\t<label>Timeout (sec) - {{updateFunctionConfiguration.Configuration.Timeout}}</label>\n\t\t\t\t<div style="margin-bottom: 15px;">\n\t\t\t\t\t<input type="number" value={{updateFunctionConfiguration.Configuration.Timeout}} style="width: 100%;">\n\t\t\t\t</div>\n\n\t\t\t\t<a class="btn btn-sm btn-primary {{#if dirty_mem || dirty_desc || dirty_timeout }}{{else}}disabled{{/if}}" on-click=\'save-desc-mem-timeout\'>Save</a>\n\n\t\t\t</div>\n\t\t</div>\n\t\t<div style="clear: both"></div>\n\n\t\t<table>\n\t\t\t<tr>\n\t\t\t\t<td>FunctionName</td>\n\t\t\t\t<td>{{getFunction.Configuration.FunctionName}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>FunctionArn</td>\n\t\t\t\t<td>{{getFunction.Configuration.FunctionArn}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>Runtime</td>\n\t\t\t\t<td>{{getFunction.Configuration.Runtime}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>Handler</td>\n\t\t\t\t<td>{{getFunction.Configuration.Handler}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>CodeSize</td>\n\t\t\t\t<td>{{getFunction.Configuration.CodeSize}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>LastModified</td>\n\t\t\t\t<td>{{getFunction.Configuration.LastModified}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>CodeSha256</td>\n\t\t\t\t<td>{{getFunction.Configuration.CodeSha256}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>Version</td>\n\t\t\t\t<td>{{getFunction.Configuration.Version}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>RevisionId</td>\n\t\t\t\t<td>{{getFunction.Configuration.RevisionId}}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>VpcConfig</td>\n\t\t\t\t<td>{{ stringify(getFunction.Configuration.VpcConfig) }}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>KMSKeyArn</td>\n\t\t\t\t<td>{{ stringify(getFunction.Configuration.KMSKeyArn) }}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>TracingConfig</td>\n\t\t\t\t<td>{{ stringify(getFunction.Configuration.TracingConfig) }}</td>\n\t\t\t</tr>\n\t\t\t<tr>\n\t\t\t\t<td>MasterArn</td>\n\t\t\t\t<td>{{ stringify(getFunction.Configuration.MasterArn) }}</td>\n\t\t\t</tr>\n\t\t</table>\n\n\n\t',
 	on: {
+		'save-desc-mem-timeout': function saveDescMemTimeout() {
+
+			var ractive = this;
+
+			lambda.updateFunctionConfiguration({
+				FunctionName: this.get('getFunction.Configuration.FunctionArn'),
+				Description: this.get('updateFunctionConfiguration.Configuration.Description'),
+				MemorySize: this.get('updateFunctionConfiguration.Configuration.MemorySize'),
+				Timeout: this.get('updateFunctionConfiguration.Configuration.Timeout')
+			}, function (err, data) {
+				if (err) return alert('Update Failed');
+
+				ractive.get_function();
+			});
+		},
+
 		init: function init() {
 			var ractive = this;
-			var params = {
-				FunctionName: this.get('function.name')
-				//Qualifier: "1"
-			};
-			lambda.getFunction(params, function (err, data) {
-				if (err) return;
 
-				ractive.set('debug', JSON.stringify(data.Configuration, null, "\t"));
-				console.log(err, data);
+			this.get_function();
+
+			this.observe('memsize', function (n) {
+				this.set('updateFunctionConfiguration.Configuration.MemorySize', sizes[n]);
+			});
+			this.observe('updateFunctionConfiguration', function (n) {
+				this.set({ dirty_mem: this.get('updateFunctionConfiguration.Configuration.MemorySize') !== this.get('getFunction.Configuration.MemorySize') });
+				this.set({ dirty_desc: this.get('updateFunctionConfiguration.Configuration.Description') !== this.get('getFunction.Configuration.Description') });
+				this.set({ dirty_timeout: this.get('updateFunctionConfiguration.Configuration.Timeout') !== this.get('getFunction.Configuration.Timeout') });
 			});
 		}
+	},
+	data: function data() {
+		return {
+			sizes: sizes,
+			dirty_mem: false,
+			dirty_desc: false,
+			dirty_timeout: false,
+
+			stringify: function stringify(v) {
+				return JSON.stringify(v, null, "\t");
+			}
+		};
+	},
+	get_function: function get_function() {
+		var ractive = this;
+
+		var params = {
+			FunctionName: this.get('function.name')
+			//Qualifier: "1"
+		};
+		lambda.getFunction(params, function (err, data) {
+			if (err) return alert('failed getting function');
+
+			ractive.set('getFunction', data);
+
+			ractive.set('updateFunctionConfiguration', {
+				Configuration: {
+					// FunctionName: 'STRING_VALUE', /* required */
+					// DeadLetterConfig: {
+					// TargetArn: 'STRING_VALUE'
+					// },
+					Description: data.Configuration.Description,
+					Environment: {
+						Variables: data.Configuration.Environment.Variables
+					},
+					// Handler: 'STRING_VALUE',
+					// KMSKeyArn: 'STRING_VALUE',
+					// Layers: [
+					// 'STRING_VALUE',
+					// /* more items */
+					// ],
+					MemorySize: data.Configuration.MemorySize,
+					// RevisionId: 'STRING_VALUE',
+					// Role: 'STRING_VALUE',
+					// Runtime: nodejs | nodejs4.3 | nodejs6.10 | nodejs8.10 | nodejs10.x | nodejs12.x | java8 | java11 | python2.7 | python3.6 | python3.7 | python3.8 | dotnetcore1.0 | dotnetcore2.0 | dotnetcore2.1 | nodejs4.3-edge | go1.x | ruby2.5 | provided,
+					Timeout: data.Configuration.Timeout
+					// TracingConfig: {
+					// Mode: Active | PassThrough
+					// },
+					// VpcConfig: {
+					// SecurityGroupIds: [
+					// 'STRING_VALUE',
+					// /* more items */
+					// ],
+					// SubnetIds: [
+					// 'STRING_VALUE',
+					// /* more items */
+					// ]
+					// }
+				}
+			});
+
+			sizes.map(function (v, idx, arr) {
+				if (v === data.Configuration.MemorySize) ractive.set('memsize', idx);
+			});
+
+			var dump = JSON.parse(JSON.stringify(data.Configuration));
+			delete dump.FunctionName;
+			delete dump.FunctionArn;
+			delete dump.Runtime;
+			delete dump.Role;
+			delete dump.Handler;
+			delete dump.CodeSize;
+			delete dump.Description;
+			delete dump.Timeout;
+			delete dump.MemorySize;
+			delete dump.LastModified;
+			delete dump.CodeSha256;
+			delete dump.Version;
+			delete dump.Environment;
+			delete dump.MasterArn;
+			delete dump.RevisionId;
+			delete dump.VpcConfig;
+			delete dump.KMSKeyArn;
+			delete dump.TracingConfig;
+
+			ractive.set('dump', JSON.stringify(dump, null, "\t"));
+
+			console.log(err, data);
+		});
 	}
 });
 
